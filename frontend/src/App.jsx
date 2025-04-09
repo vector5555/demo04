391cd016a5c977d06d0a632900aa454a3ede6c88
vector5555 <55052675+vector5555@users.noreply.github.com>
2025-04-09 18:36:11 +0800

    备份20250409

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BasicLayout from './layouts/BasicLayout';
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/dashboard';
 import Users from './pages/admin/Users';
 import Roles from './pages/admin/Roles';
 import RoleUser from './pages/admin/RoleUser';
